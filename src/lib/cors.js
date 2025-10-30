c2a124d262ea5daf3e270c57236a94c9b121c972
sarah <sarahibrahimabdelhamid@gmail.com>
2025-10-30 05:56:15 +0300

    adding credintials

--- src/lib/cors.js
+++ src/lib/cors.js
@@ -1,5 +1,9 @@
 export function applyCors(response) {
-  response.headers.set('Access-Control-Allow-Origin', '*')
+  const origin = process.env.NODE_ENV === 'production'
+    ? 'https://next-mini-blog-6zdo.vercel.app'
+    : 'http://localhost:3000'
+
+  response.headers.set('Access-Control-Allow-Origin', origin)
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   response.headers.set('Access-Control-Allow-Credentials', 'true')
