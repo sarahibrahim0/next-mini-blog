fa5f08a617fd08c6632d5698b8b15ac9c4f62b91
sarah <sarahibrahimabdelhamid@gmail.com>
2025-10-30 03:05:47 +0300

    updates

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const ARTICLE_PER_PAGE = 6;
 
-const PRODUCTION_DOMAIN = "http://localhost:3000";
+const PRODUCTION_DOMAIN = "https://next-mini-blog.vercel.app";
 const DEVELOPMENT_DOMAIN = "http://localhost:3000";
 
 export const DOMAIN = process.env.NODE_ENV === 'production' 
