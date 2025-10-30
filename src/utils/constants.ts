cbc4c93b0d78e58aa93240355e8003f4e0726d2a
sarah <sarahibrahimabdelhamid@gmail.com>
2025-10-30 08:21:05 +0300

    editing gnerateToken

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,8 @@
 export const ARTICLE_PER_PAGE = 6;
 
-const PRODUCTION_DOMAIN = "https://next-mini-blog.vercel.app";
+const PRODUCTION_DOMAIN = "https://next-mini-blog-6zdo.vercel.app";
 const DEVELOPMENT_DOMAIN = "http://localhost:3000";
 
-export const DOMAIN = process.env.NODE_ENV === 'production' 
+export const DOMAIN = process.env.NODE_ENV === 'production'
     ? PRODUCTION_DOMAIN
     : DEVELOPMENT_DOMAIN;
